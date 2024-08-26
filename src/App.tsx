9e6e6bd5e1362d9ea31c6de898fa8e439156f7cc
Ijeoma Onuaju <ijeomaonuajurx@gmail.com>
2024-08-26 17:21:59 +0100

    images

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 // import { useState } from 'react'
 // import './App.css'
 import Home from './pages/Home.tsx'
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-
+// import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
 
